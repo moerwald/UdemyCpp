5c7391478fb4f200bf914848dee98a68cbc2d653
moerwald <andreas.mewald@gmail.com>
2021-06-01 12:37:10 +0200

    Added datatype method

--- src/1_CppGrundlagen/Main.cc
+++ src/1_CppGrundlagen/Main.cc
@@ -1,8 +1,41 @@
 #include <iostream>
 
+int AddVars()
+{
+    int zahl1{};
+    int zahl2{};
+    int ergebnis;
+
+    ergebnis = zahl1 + zahl2;
+
+    return ergebnis;
+}
+
+void DataTypes()
+{
+    // bools are represented as numeric value. True = 1, false = 0;
+    bool boolIsFalse = false;
+    auto boolIsTrue = !boolIsFalse;
+
+    std::cout << boolIsFalse << std::endl;
+    std::cout << boolIsTrue << std::endl;
+
+    // chars represent single letters, for UTF other types are needed
+    auto letter = 'a';
+    std::cout << letter << std::endl;
+
+    unsigned char uLetter = 97;
+    std::cout << uLetter << std::endl;
+
+}
+
 int main(void)
 {
 	std::cout << "Hello World!" << std::endl;
+    AddVars();
+    DataTypes();
 
 	return 0;
 }
+
+
