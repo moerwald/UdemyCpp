8d46a8eba33c1fc50ace7fa8406560977eae423c
moerwald <andreas.mewald@gmail.com>
2021-06-09 14:28:06 +0200

    Added white spaces

--- src/2_ArrayGrundlagen/CallByValue.cpp
+++ src/2_ArrayGrundlagen/CallByValue.cpp
@@ -6,16 +6,16 @@ using std::endl;
 
 void f(int* p_number)
 {
-    cout << "f: p_number" << p_number << endl;
-    cout << "f: &p_number" << &p_number << endl;
+    cout << "f: p_number " << *p_number << endl;
+    cout << "f: &p_number " << &p_number << endl;
 }
 
 int CallByValue::main()
 {
     int* p_number = new int{ 4 };
 
-    cout << "MAIN: p_number" << p_number << endl;
-    cout << "MAIN: &p_number" << &p_number << endl;
+    cout << "MAIN: p_number " << p_number << endl;
+    cout << "MAIN: &p_number " << &p_number << endl;
     
     f(p_number);
 
