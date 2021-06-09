293d5c75ff6ecd2d5828e3bf925e508af20f6b0a
moerwald <andreas.mewald@gmail.com>
2021-06-09 14:44:51 +0200

    Added new class.

--- src/2_ArrayGrundlagen/CallByReference2.cpp
+++ src/2_ArrayGrundlagen/CallByReference2.cpp
@@ -5,17 +5,25 @@ using std::cout;
 using std::endl;
 
 
-void f1()
+// call by value
+void f1(int number)
 {
-
+    number++;
 }
 
-void f2()
+// call by reference
+void f2(int &number)
 {
-
+    number++;
 }
 
 int CallByReference2::main()
 {
+    int num{ 5 };
+    cout << "num " << num << endl;
+    f1(num);
+    cout << "num " << num << endl;
+    f2(num);
+    cout << "num " << num << endl;
     return 0;
 }
--- src/2_ArrayGrundlagen/Main.cpp
+++ src/2_ArrayGrundlagen/Main.cpp
@@ -2,10 +2,12 @@
 #include "Pointer1.h"
 #include "CallByValue.h"
 #include "CallByReference.h"
+#include "CallByReference2.h"
 
 int main()
 {
-    return CallByReference::main();
+    return CallByReference2::main();
+    //return CallByReference::main();
     //return CallByValue::main();
     //return Pointer1::main();
     //return Pointer4::main();
