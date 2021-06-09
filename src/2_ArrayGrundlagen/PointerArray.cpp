1a462e6621c5f8b4d8c101de3fac91323dabe86d
moerwald <andreas.mewald@gmail.com>
2021-06-09 14:56:12 +0200

    Added new method

--- src/2_ArrayGrundlagen/PointerArray.cpp
+++ src/2_ArrayGrundlagen/PointerArray.cpp
@@ -4,6 +4,21 @@
 using std::cout;
 using std::endl;
 
+int getArrayMax(int inputArray[])
+{
+    auto currentMax = 0;
+    for (size_t i = 0; i < sizeof(inputArray)/sizeof(int); i++)
+    {
+        cout << inputArray[i] << endl;
+        if (inputArray[i] > currentMax)
+        {
+            currentMax = inputArray[i]
+        }
+    }
+
+    return currentMax;
+}
+
 int PointerArray::main()
 {
     const unsigned int array_size = 10;
@@ -21,6 +36,10 @@ int PointerArray::main()
         cout << p[i] << endl;
         cout << &p[i] << endl;
     }
+
+    auto max = getArrayMax(p);
+    cout << "max: " << max << endl;
+
     // headp deallocation
     delete[] p;
 
