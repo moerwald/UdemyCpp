29b168164c4657e173f762b2fcb093dcb4a7ef4b
moerwald <andreas.mewald@gmail.com>
2021-06-09 14:41:24 +0200

    Added new class

---
+++ src/2_ArrayGrundlagen/CallByReference2.cpp
@@ -0,0 +1,21 @@
+#include "CallByReference2.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+void f1()
+{
+
+}
+
+void f2()
+{
+
+}
+
+int CallByReference2::main()
+{
+    return 0;
+}
---
+++ src/2_ArrayGrundlagen/CallByReference2.h
@@ -0,0 +1,6 @@
+#pragma once
+class CallByReference2
+{
+public: int main();
+};
+
