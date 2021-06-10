b12f1a29958bc1597fee7a19ac694ecaf15afbe2
moerwald <andreas.mewald@gmail.com>
2021-06-10 17:36:19 +0200

    Added additional output

--- src/3_WeitereGrundlagen/Struct1.cpp
+++ src/3_WeitereGrundlagen/Struct1.cpp
@@ -11,12 +11,18 @@ struct Movie
 int Struct1::Main()
 {
     Movie starwars7{ 2015, "Star Wars the Force Awakens", "J. J. Abrams" };
-
     std::cout << starwars7.year << std::endl;
     std::cout << starwars7.title << std::endl;
     std::cout << starwars7.director << std::endl;
 
     Movie starwars8{ 2017, "Star Wars the last Jedi", "Rian Johnson" };
+    std::cout << starwars8.year << std::endl;
+    std::cout << starwars8.title << std::endl;
+    std::cout << starwars8.director << std::endl;
+
     Movie starwars9{ 2019, "Star Wars the Rise of the Skywalker", "J. J. Abrams" };
+    std::cout << starwars9.year << std::endl;
+    std::cout << starwars9.title << std::endl;
+    std::cout << starwars9.director << std::endl;
     return 0;
 }
