061124dea918c5cc42eb4403829dbbadf7ec03d7
moerwald <andreas.mewald@gmail.com>
2021-06-10 17:39:06 +0200

    Added print functions

--- src/3_WeitereGrundlagen/Struct1.cpp
+++ src/3_WeitereGrundlagen/Struct1.cpp
@@ -8,21 +8,31 @@ struct Movie
     const char* director;
 };
 
+
+void PrintMovie(Movie& movie)
+{
+    std::cout << movie.year << std::endl;
+    std::cout << movie.title << std::endl;
+    std::cout << movie.director << std::endl;
+}
+
+void PrintMovie(Movie* movie)
+{
+    std::cout << movie->year << std::endl;
+    std::cout << movie->title << std::endl;
+    std::cout << movie->director << std::endl;
+}
+
 int Struct1::Main()
 {
     Movie starwars7{ 2015, "Star Wars the Force Awakens", "J. J. Abrams" };
-    std::cout << starwars7.year << std::endl;
-    std::cout << starwars7.title << std::endl;
-    std::cout << starwars7.director << std::endl;
+    PrintMovie(starwars7);
 
     Movie starwars8{ 2017, "Star Wars the last Jedi", "Rian Johnson" };
-    std::cout << starwars8.year << std::endl;
-    std::cout << starwars8.title << std::endl;
-    std::cout << starwars8.director << std::endl;
+    PrintMovie(&starwars8);
 
     Movie starwars9{ 2019, "Star Wars the Rise of the Skywalker", "J. J. Abrams" };
-    std::cout << starwars9.year << std::endl;
-    std::cout << starwars9.title << std::endl;
-    std::cout << starwars9.director << std::endl;
+    PrintMovie(&starwars9);
+
     return 0;
 }
