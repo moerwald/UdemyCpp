55bb5fe3f8185c8dd6610f03bc4a27f9f099ef22
moerwald <andreas.mewald@gmail.com>
2021-06-11 17:39:56 +0200

    Moved movie to separate namespace

--- src/3_WeitereGrundlagen/Struct1.cpp
+++ src/3_WeitereGrundlagen/Struct1.cpp
@@ -1,22 +1,24 @@
 #include "Struct1.h"
 #include <iostream>
 
-struct Movie
+namespace Cinema
 {
-    const int year;
-    const char* title;
-    const char* director;
-};
+    struct Movie
+    {
+        const int year;
+        const char* title;
+        const char* director;
+    };
+}
 
-
-void PrintMovie(Movie& movie)
+void PrintMovie(Cinema::Movie& movie)
 {
     std::cout << movie.year << std::endl;
     std::cout << movie.title << std::endl;
     std::cout << movie.director << std::endl;
 }
 
-void PrintMovie(Movie* movie)
+void PrintMovie(Cinema::Movie* movie)
 {
     std::cout << movie->year << std::endl;
     std::cout << movie->title << std::endl;
@@ -25,13 +27,13 @@ void PrintMovie(Movie* movie)
 
 int Struct1::Main()
 {
-    Movie starwars7{ 2015, "Star Wars the Force Awakens", "J. J. Abrams" };
+    Cinema::Movie starwars7{ 2015, "Star Wars the Force Awakens", "J. J. Abrams" };
     PrintMovie(starwars7);
 
-    Movie starwars8{ 2017, "Star Wars the last Jedi", "Rian Johnson" };
+    Cinema::Movie starwars8{ 2017, "Star Wars the last Jedi", "Rian Johnson" };
     PrintMovie(&starwars8);
 
-    Movie starwars9{ 2019, "Star Wars the Rise of the Skywalker", "J. J. Abrams" };
+    Cinema::Movie starwars9{ 2019, "Star Wars the Rise of the Skywalker", "J. J. Abrams" };
     PrintMovie(&starwars9);
 
     return 0;
