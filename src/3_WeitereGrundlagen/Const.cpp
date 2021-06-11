9f0d51f649575ef6b5241d1c848f2180ee9d06fe
moerwald <andreas.mewald@gmail.com>
2021-06-11 18:10:27 +0200

    Added constexpr

--- src/3_WeitereGrundlagen/Const.cpp
+++ src/3_WeitereGrundlagen/Const.cpp
@@ -1,7 +1,22 @@
 #include "Const.h"
+#include <iostream>
+
+void printValue(const unsigned int value)
+{
+    std::cout << value << std::endl;
+    // Compiler creates error since const value can't be changed!
+    // value = 10;
+}
 
 int Const::Main()
 {
     const int Len = 10;
+
+    int arr[Len]{ 1,2,3,4 };
+
+    // Compiler calculates value during compile stage!!!
+    constexpr int Len2 = 5 * 2;
+    int arr2[Len2] = { 1,2,3,4 };
+    
     return 0;
 }
--- src/3_WeitereGrundlagen/Main.cpp
+++ src/3_WeitereGrundlagen/Main.cpp
@@ -3,12 +3,14 @@
 #include "Struct2.h"
 #include "Enum.h"
 #include "Defines.h"
+#include "Const.h"
 
 int main( )
 {
     //return Enum::Main();
     //return Struct1::Main();
     //return Struct2::Main();
-    return Defines::Main();
+    //return Defines::Main();
+    return Const::Main();
 
 }
