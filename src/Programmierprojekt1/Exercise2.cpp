05f17ddc42bb4c9313e538e1510c3e437459a913
moerwald <andreas.mewald@gmail.com>
2021-06-12 19:53:14 +0200

    Added new files

---
+++ src/Programmierprojekt1/Exercise2.cpp
@@ -0,0 +1,9 @@
+#include "Exercise2.h"
+
+void Exercise2::PushBack(int*& inputArray, const unsigned int& size, const int& value)
+{
+}
+
+void Exercise2::PopBack(int*& inputArray, const unsigned int& size)
+{
+}
---
+++ src/Programmierprojekt1/Exercise2.h
@@ -0,0 +1,7 @@
+#pragma once
+class Exercise2
+{
+public: static void PushBack(int*& inputArray, const unsigned int& size, const int& value);
+public: static void PopBack(int*& inputArray, const unsigned int& size);
+};
+
