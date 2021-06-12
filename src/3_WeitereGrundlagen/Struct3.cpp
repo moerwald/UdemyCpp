717e95d3810158d76200e6bd9640bde8b8ab7b79
moerwald <andreas.mewald@gmail.com>
2021-06-12 19:35:28 +0200

    Moved enum to separate namespace

--- src/3_WeitereGrundlagen/Struct3.cpp
+++ src/3_WeitereGrundlagen/Struct3.cpp
@@ -1,52 +1,56 @@
 #include "Struct3.h"
 #include <iostream>
 
-enum class Lanes : int
+namespace NS_Struct3
 {
-    RIGHT = 1,
-    CENTER = 0,
-    LEFT = -1,
-};
 
-typedef struct Vehicle
-{
-    int id;
-    Lanes lane;
-    float velocity;
-
-    Vehicle(Vehicle& rhs)
+    enum class Lanes : int
     {
-        id = rhs.id;
-        lane = rhs.lane;
-        velocity = rhs.velocity;
-    }
+        RIGHT = 1,
+        CENTER = 0,
+        LEFT = -1,
+    };
 
-    Vehicle(int id, Lanes lane, float velocity)
+    typedef struct Vehicle
     {
-        this->id = id;
-        this->lane = lane;
-        this->velocity = velocity;
-    }
+        int id;
+        Lanes lane;
+        float velocity;
 
-    void Print()
-    {
-        std::cout << "ID: " << id << std::endl;
-        std::cout << "LANE: " << static_cast<int>(lane) << std::endl;
-        std::cout << "VELOCITY: " << velocity << std::endl;
-    }
-} TS_Vehicle;
+        Vehicle(Vehicle& rhs)
+        {
+            id = rhs.id;
+            lane = rhs.lane;
+            velocity = rhs.velocity;
+        }
 
-void Print(TS_Vehicle v) {
-    std::cout << "ID: " << v.id << std::endl;
-    std::cout << "LANE: " << static_cast<int>(v.lane) << std::endl;
-    std::cout << "VELOCITY: " << v.velocity << std::endl;
+        Vehicle(int id, Lanes lane, float velocity)
+        {
+            this->id = id;
+            this->lane = lane;
+            this->velocity = velocity;
+        }
+
+        void Print()
+        {
+            std::cout << "ID: " << id << std::endl;
+            std::cout << "LANE: " << static_cast<int>(lane) << std::endl;
+            std::cout << "VELOCITY: " << velocity << std::endl;
+        }
+    } TS_Vehicle;
+
+    void Print(TS_Vehicle v) {
+        std::cout << "ID: " << v.id << std::endl;
+        std::cout << "LANE: " << static_cast<int>(v.lane) << std::endl;
+        std::cout << "VELOCITY: " << v.velocity << std::endl;
+    }
 }
 
 int Struct3::Main()
 {
-    TS_Vehicle v1{ 1, Lanes::CENTER, 48.5f };
-    TS_Vehicle v2{ 2, Lanes::RIGHT, 100.0f };
-    TS_Vehicle v3{ 3, Lanes::LEFT, 42.0f };
+    NS_Struct3::TS_Vehicle v1{ 1, NS_Struct3::Lanes::CENTER, 48.5f };
+    NS_Struct3::TS_Vehicle v2{ 2, NS_Struct3::Lanes::RIGHT, 100.0f };
+    NS_Struct3::TS_Vehicle v3{ 3, NS_Struct3::Lanes::LEFT, 42.0f };
 
     return 0;
 }
