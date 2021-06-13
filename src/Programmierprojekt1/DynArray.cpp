422cdb33f1f70f57f16737c47f3b2acb8656f81a
moerwald <andreas.mewald@gmail.com>
2021-06-13 19:55:28 +0200

    Added vscode files.

--- src/Programmierprojekt1/DynArray.cpp
+++ src/Programmierprojekt1/DynArray.cpp
@@ -1,9 +1,32 @@
 #include "DynArray.h"
 
-void DynArray::PushBack(int*& inputArray, const unsigned int& size, const int& value)
+void DynArray::PushBack(int *&inputArray, const unsigned int &size, const int &value)
 {
+    int *temp = nullptr;
+    temp = new int[size + 1];
+
+    for (size_t i = 0; i < size; i++)
+    {
+        temp[i] = inputArray[i];
+    }
+
+    temp[size] = value;
+
+    delete[] inputArray;
+    inputArray = temp;
 }
 
-void DynArray::PopBack(int*& inputArray, const unsigned int& size)
+void DynArray::PopBack(int *&inputArray, const unsigned int &size)
 {
+    int *temp = nullptr;
+    temp = new int[size - 1];
+
+    for (size_t i = 0; i < size; i++)
+    {
+        /* code */
+        temp[i] = inputArray[i];
+    }
+
+    delete[] inputArray;
+    inputArray = temp;
 }
--- src/Programmierprojekt1/Main.cpp
+++ src/Programmierprojekt1/Main.cpp
@@ -1,6 +1,24 @@
 #include "DynArray.h"
+#include <iostream>
 
 int main()
 {
+    int* myData = nullptr;
+
+    const unsigned int size = 3;
+
+    int a[size];
+    myData = &(a[0]);
+
+    myData[0] = 0;
+    myData[1] = 1;
+    myData[2] = 2;
+
+    std::cout << "Start-values of the array " << std::endl;
+    for (size_t i = 0; i < size; i++)
+    {
+        std::cout << myData[i] << std::endl;
+    }
+
     return 0;
 }
