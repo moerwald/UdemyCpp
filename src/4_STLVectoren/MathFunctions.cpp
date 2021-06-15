c014adafe0c4399ef1f0c45b7de0db38500ee24f
moerwald <andreas.mewald@gmail.com>
2021-06-15 19:57:41 +0200

    Implemented FillDoubleVector and PrintDoubleVector

--- src/4_STLVectoren/MathFunctions.cpp
+++ src/4_STLVectoren/MathFunctions.cpp
@@ -1,11 +1,21 @@
 #include "MathFunctions.h"
+#include <iostream>
 
 void MathFunctions::FillDoubleVector(std::vector<double>&& vec)
 {
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        vec.push_back(i);
+    }
 }
 
 void MathFunctions::PrintDoubleVector(std::vector<double>&& vec)
 {
+    std::cout << "PrintDoubleVector" << std::endl;
+    for (size_t i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i] << std::endl;
+    }
 }
 
 void MathFunctions::DoubleVectorExp(std::vector<double>&& vec)
