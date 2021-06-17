c20aa682f831894f3bb8965e74a1f9865bda929d
moerwald <andreas.mewald@gmail.com>
2021-06-17 16:43:15 +0200

    Added comment

--- src/4_STLVectoren/Vector1.cpp
+++ src/4_STLVectoren/Vector1.cpp
@@ -29,6 +29,7 @@ int Vector1::Main()
     std::cout << "(*iv4)[0] " << (*iv4)[0] << std::endl;
     std::cout << "*(iv.begin()) " << *(iv.begin()) << std::endl;
 
+    // Remove 43, the whole vector is shifted after the operation vector includes 42, 44, 45, 45
     auto deleteFrom = std::remove(iv.begin(), iv.end(), 43);
     for (auto i : iv)
     {
