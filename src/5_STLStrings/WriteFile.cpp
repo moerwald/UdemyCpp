f510bbdc1bc2b4931f2e91826562db14c92eb4d4
moerwald <andreas.mewald@gmail.com>
2021-06-20 17:10:19 +0200

    Now reading and writing file.

--- src/5_STLStrings/WriteFile.cpp
+++ src/5_STLStrings/WriteFile.cpp
@@ -8,17 +8,35 @@ int WriteFile::main()
 
     std::ifstream stream;
     stream.open("Text.txt");
+    std::string file_content;
     if (stream.is_open())
     {
-        std::string file_content;
         std::string str;
 
-        stream >> file_content;
+        while (std::getline(stream, str))
+            file_content += str + "\n";
 
         std::cout << std::endl << "===============================" << std::endl;
         std::cout << "Full content: " << std::endl << file_content;
     }
-
     stream.close();
+
+    std::string search_str = "one";
+    auto replace_str = "xxxxx";
+
+    auto idx = file_content.find(search_str);
+    if (idx != std::string::npos)
+    {
+        file_content.replace(idx, search_str.size(), replace_str);
+
+        std::ofstream ofstream;
+        ofstream.open("TextOutput.txt");
+        if (ofstream.is_open())
+        {
+            ofstream << file_content;
+        }
+        ofstream.close();
+    }
+
     return 0;
 }
