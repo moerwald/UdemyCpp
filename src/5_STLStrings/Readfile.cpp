038217a01514c86c48e81e6a85999d7700baa52e
moerwald <andreas.mewald@gmail.com>
2021-06-20 16:45:50 +0200

    Added Readfile

---
+++ src/5_STLStrings/Readfile.cpp
@@ -0,0 +1,25 @@
+#include "Readfile.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+
+int Readfile::main()
+{
+    std::ifstream stream;
+    stream.open("Text.txt");
+    if (stream.is_open())
+    {
+        std::string file_content;
+        std::string str;
+        while (std::getline(stream, str))
+        {
+            std::cout << str << std::endl;
+            file_content += str;
+        }
+
+        std::cout << "Full content: " << std::endl << file_content;
+    }
+
+    stream.close();
+    return 0;
+}
---
+++ src/5_STLStrings/Readfile.h
@@ -0,0 +1,6 @@
+#pragma once
+class Readfile
+{
+public: static int main();
+};
+
