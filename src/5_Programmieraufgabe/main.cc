b931b1e477d9d04a799e0549bce4f21f8d50ac8f
moerwald <andreas.mewald@gmail.com>
2021-06-20 14:36:20 +0200

    Added first implementation of main

--- src/5_Programmieraufgabe/main.cc
+++ src/5_Programmieraufgabe/main.cc
@@ -1,6 +1,25 @@
+#include "exercise.h"
+#include <iostream>
+
 int main()
 {
+    Matrix matrix{
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    for (size_t i = 0; i < matrix.size(); i++)
+    {
+        for (size_t j = 0; i < matrix[i].size(); i++)
+        {
+            std::cout << "[" << i << "][" << j << "]: " << matrix[i][j] << std::endl;
+        }
+    }
+
+    auto max_row_vals = maw_row_values(matrix);
 
 
-	return 0;
+
+    return 0;
 }
