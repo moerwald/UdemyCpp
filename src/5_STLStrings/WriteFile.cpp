d63473a227f4ec63dd391d3f56a78814d93e5eaa
moerwald <andreas.mewald@gmail.com>
2021-06-20 16:57:36 +0200

    First implementation

--- src/5_STLStrings/WriteFile.cpp
+++ src/5_STLStrings/WriteFile.cpp
@@ -1,6 +1,24 @@
 #include "WriteFile.h"
+#include <fstream>
+#include <iostream>
+#include <string>
 
 int WriteFile::main()
 {
+
+    std::ifstream stream;
+    stream.open("Text.txt");
+    if (stream.is_open())
+    {
+        std::string file_content;
+        std::string str;
+
+        stream >> file_content;
+
+        std::cout << std::endl << "===============================" << std::endl;
+        std::cout << "Full content: " << std::endl << file_content;
+    }
+
+    stream.close();
     return 0;
 }
--- src/5_STLStrings/main.cpp
+++ src/5_STLStrings/main.cpp
@@ -1,8 +1,10 @@
 #include "String1.h"
 #include "Readfile.h"
+#include "WriteFile.h"
 
 int main()
 {
     // return String1::main();
-    return Readfile::main();
+    // return Readfile::main();
+    return WriteFile::main();
 }
