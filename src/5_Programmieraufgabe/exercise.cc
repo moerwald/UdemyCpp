9d4c249d404c74db7589015ec538f2f3761cab9b
moerwald <andreas.mewald@gmail.com>
2021-06-20 14:31:07 +0200

    Added methods

--- src/5_Programmieraufgabe/exercise.cc
+++ src/5_Programmieraufgabe/exercise.cc
@@ -1,1 +1,26 @@
 
+#include "exercise.h"
+
+std::vector<double> maw_row_values(Matrix& matrix)
+{
+    std::vector<double> max_vec;
+
+    for (size_t i = 0; i < matrix.size(); i++)
+    {
+        std::vector<double> row = matrix[i];
+        auto max = 0.0;
+
+        for (auto val : row)
+            if (val > max)
+                max = val;
+
+        max_vec.push_back(max);
+    }
+
+    return max_vec;
+}
+
+double sort_and_max(std::vector<double>& vec)
+{
+    return 0.0;
+}
--- src/5_Programmieraufgabe/exercise.h
+++ src/5_Programmieraufgabe/exercise.h
@@ -1,1 +1,9 @@
 #pragma once
+
+#include <vector>
+
+using Matrix = std::vector<std::vector<double>>;
+
+std::vector<double> maw_row_values(Matrix& matrix);
+
+double sort_and_max(std::vector<double>& vec);
