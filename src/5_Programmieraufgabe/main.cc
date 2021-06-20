9bc427464b181caa5490ea846956ba258120bcab
moerwald <andreas.mewald@gmail.com>
2021-06-20 14:43:45 +0200

    Formating

--- src/5_Programmieraufgabe/main.cc
+++ src/5_Programmieraufgabe/main.cc
@@ -10,22 +10,14 @@ int main()
     };
 
     for (size_t i = 0; i < matrix.size(); i++)
-    {
         for (size_t j = 0; j < matrix[i].size(); j++)
-        {
             std::cout << "[" << i << "][" << j << "]: " << matrix[i][j] << std::endl;
-        }
-    }
 
     auto max_row_vec = maw_row_values(matrix);
     for (auto val : max_row_vec)
         std::cout << "Max in row: " << val << std::endl;
 
     std::cout << "Overall max: " << sort_and_max(max_row_vec) << std::endl;
-
-
-
-
 
     return 0;
 }
