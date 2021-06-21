e4d92fc1ee5bcb209bbe048cebc2323e589c3eaf
moerwald <andreas.mewald@gmail.com>
2021-06-21 05:18:24 +0200

    Added clear call

--- src/5_STLStrings/String3.cpp
+++ src/5_STLStrings/String3.cpp
@@ -1,6 +1,17 @@
 #include "String3.h"
+#include <string>
+#include <iostream>
 
 int String3::main()
 {
+    std::string str = "This is a string";
+
+    std::string str2 = str;
+    std::cout << "str2: " << str2 << std::endl;
+
+    // Empty the string
+    str2.clear();
+    std::cout << "str2.clear(): " << str2 << std::endl;
+
     return 0;
 }
