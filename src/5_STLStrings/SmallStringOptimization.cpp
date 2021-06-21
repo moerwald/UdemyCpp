05f6cd9f9d7bbeb433eb800e61aa140bc7de4964
moerwald <andreas.mewald@gmail.com>
2021-06-21 15:09:13 +0200

    Finished

--- src/5_STLStrings/SmallStringOptimization.cpp
+++ src/5_STLStrings/SmallStringOptimization.cpp
@@ -1,15 +1,22 @@
 #include "SmallStringOptimization.h"
 #include <string>
 #include <iostream>
+#include <cstdlib>
 
+void* operator new(std::size_t n)
+{
+    std::cout << "Allocating " << n << " bytes on the heap" << std::endl;
+    return malloc(n);
+}
 
 int SmallStringOptimization::main()
 {
-
-    for (size_t i = 0; i < 32; i++)
+    const std::size_t nr_of_strings = 64;
+    // Build and run under RELEASE!!! -> string with size < 16 are allocated on the heap
+    for (size_t i = 0; i < nr_of_strings; i++)
     {
         std::string str(i, 'a');
-        std::cout << str << std::endl;
+        std::cout << i << " " << str << std::endl;
     }
     return 0;
 }
