0f9ce6a71a9258ccc1042c1d8ee44a07c4b5d76d
moerwald <andreas.mewald@gmail.com>
2021-06-22 14:04:54 +0200

    Added methods.

--- src/3_WeitereGrundlagen/RAndLValueSamples.cpp
+++ src/3_WeitereGrundlagen/RAndLValueSamples.cpp
@@ -1,6 +1,43 @@
 #include "RAndLValueSamples.h"
+#include<string>
+#include<iostream>
+
+void print_l_value_reference(std::string& str)
+{
+    std::cout << "print_l_value_reference: " << str << std::endl;
+}
+
+void print_const_l_value_reference(const std::string& str)
+{
+    std::cout << "print_const_l_value_reference: " << str << std::endl;
+}
+
+void print_r_value_reference(std::string&& str)
+{
+    std::cout << "print_r_value_reference: " << str << std::endl;
+}
 
 int RAndLValueSamples::main()
 {
+    std::string str = "test";
+    std::string& str_l_value_ref = str;
+    std::string* str_l_value_ptr = &str;
+
+    print_l_value_reference(str);
+
+    const std::string str_const = "test";
+    print_const_l_value_reference(str_const);
+    print_const_l_value_reference(str);
+    print_const_l_value_reference(str_l_value_ref);
+    print_const_l_value_reference(*str_l_value_ptr);
+
+    print_r_value_reference("test");
+
+    std::string str1 = "str1";
+    std::string str2 = "str2";
+    std::string str12 = str1 + str2;
+    print_r_value_reference(str1 + str2);
+    // Won't compile print_r_value_reference(str12); 
+
     return 0;
 }
