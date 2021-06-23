1330c76455cec5ea904c20229d307def0aac3018
moerwald <andreas.mewald@gmail.com>
2021-06-23 11:21:10 +0200

    Added sample

--- src/5_STLStrings/StringStream1.cpp
+++ src/5_STLStrings/StringStream1.cpp
@@ -1,6 +1,23 @@
 #include "StringStream1.h"
+#include <string>
+#include <sstream>
+#include <iostream>
 
 int StringStream1::main()
 {
+    std::string birthday;
+
+    std::cout << "Please enter your birthday (DD.MM.YYYY): ";
+    std::cin >> birthday;
+
+    std::stringstream birthday_ss(birthday);
+
+    unsigned int day, month, year;
+    char delimiter;
+
+    birthday_ss >> day >> delimiter >> month >> delimiter >> year;
+
+    std::cout << "Your birthday is: " << birthday << std::endl;
+
     return 0;
 }
