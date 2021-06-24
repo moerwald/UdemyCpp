3399354cc40e9941f747a709ed681f91a00ac3f7
moerwald <andreas.mewald@gmail.com>
2021-06-24 11:37:00 +0200

    Created array and iterating over it

--- src/6_STLContainerUndIteratoren/StdArray.cpp
+++ src/6_STLContainerUndIteratoren/StdArray.cpp
@@ -1,6 +1,22 @@
 #include "StdArray.h"
+#include <array>
+#include <numeric>
+#include <iostream>
 
 int StdArray::main()
 {
+
+    std::array<int, 10> my_array; // Creates an array with 10 entries, size is FIXED!!!
+
+    std::iota(my_array.begin(), my_array.end(), 1); // Fill up with increasing ints
+
+    std::cout << "Iterate via foreach" << std::endl;
+    for (auto it : my_array)
+    {
+        std::cout << it << std::endl;
+    }
+
+    
+
     return 0;
 }
