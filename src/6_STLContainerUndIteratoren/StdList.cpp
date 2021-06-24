6321c0880be273511e0f3a58a3883cd292d723e0
moerwald <andreas.mewald@gmail.com>
2021-06-24 11:45:50 +0200

    Implemented List::main

--- src/6_STLContainerUndIteratoren/StdList.cpp
+++ src/6_STLContainerUndIteratoren/StdList.cpp
@@ -1,6 +1,25 @@
 #include "StdList.h"
+#include <list>
+#include <iostream>
 
 int StdList::main()
 {
+    std::list<int> my_list = { 1,2,3 };
+    for (auto it = my_list.begin(); it != my_list.end(); ++it)
+    {
+        std::cout << *it << " " << &(*it) << std::endl;
+    }
+
+    std::cout << std::endl;
+
+    my_list.push_front(0);
+    my_list.push_back(4);
+    for (auto it = my_list.begin(); it != my_list.end(); ++it)
+    {
+        std::cout << *it << " " << &(*it) << std::endl;
+    }
+
+    std::cout << std::endl;
+
     return 0;
 }
