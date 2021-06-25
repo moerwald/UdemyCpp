109cef2ad4996c056fbf4012a6e63fa22a837b8d
moerwald <andreas.mewald@gmail.com>
2021-06-25 19:42:47 +0200

    Added new samples

--- src/6_STLContainerUndIteratoren/StdMap.cpp
+++ src/6_STLContainerUndIteratoren/StdMap.cpp
@@ -8,7 +8,7 @@ void print_map(const std::map<std::string, int>& map)
 {
     for (auto kvp : map)
     {
-        std::cout << "Key: " << kvp.first << " Value: " << kvp.second;
+        std::cout << "Key: " << kvp.first << " Value: " << kvp.second << std::endl;
     }
 }
 
@@ -21,8 +21,22 @@ int StdMap::main()
 
     print_map(my_map);
 
+    auto it = my_map.find("not existing");
+    bool exists = it != my_map.end();
+    std::cout << "Exists: " << exists << std::endl;
     
+    std::map<std::string, int> my_map2 = {
+        {"st1", 1},
+        {"st2", 2},
+        {"henlo", 4} // Duplicate values are not merged.
+    };
 
+    my_map.merge(my_map2);
+    print_map(my_map);
+
+    std::cout << "Ereasing st1 from my_map" << std::endl;
+    my_map.erase(my_map.find("st1"));
+    print_map(my_map);
 
     return 0;
 }
