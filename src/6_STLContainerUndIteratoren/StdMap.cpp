d5aeea2fb77990aa2ad0a6e70b990896e4d35a04
moerwald <andreas.mewald@gmail.com>
2021-06-25 19:37:42 +0200

    Now printing map

--- src/6_STLContainerUndIteratoren/StdMap.cpp
+++ src/6_STLContainerUndIteratoren/StdMap.cpp
@@ -1,6 +1,28 @@
 #include "StdMap.h"
+#include <map>
+#include <string>
+#include <iostream>
+
+
+void print_map(const std::map<std::string, int>& map)
+{
+    for (auto kvp : map)
+    {
+        std::cout << "Key: " << kvp.first << " Value: " << kvp.second;
+    }
+}
 
 int StdMap::main()
 {
+    std::map<std::string, int> my_map;
+    const std::string test = "test";
+    my_map[test] = 1;
+    my_map["henlo"] = 2;
+
+    print_map(my_map);
+
+    
+
+
     return 0;
 }
