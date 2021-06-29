5a053ea1d36055aea56cca9cb76aea61f6086509
moerwald <andreas.mewald@gmail.com>
2021-06-29 10:39:29 +0200

    Now using StdIterators::main

--- src/6_STLContainerUndIteratoren/main.cpp
+++ src/6_STLContainerUndIteratoren/main.cpp
@@ -1,9 +1,11 @@
 
 #include "StdArray.h"
 #include "StdList.h"
+#include "StdIterators.h"
 
 int main()
 {
     //return StdArray::main();
-    return StdList::main();
+    //return StdList::main();
+    return StdIterators::main();
 }
