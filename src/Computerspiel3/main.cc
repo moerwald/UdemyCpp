f7027a48baa8d6240f83ac22672ea2491ba45700
moerwald <andreas.mewald@gmail.com>
2021-07-01 14:28:18 +0200

    Game now supports 2D game board.

--- src/Computerspiel3/main.cc
+++ src/Computerspiel3/main.cc
@@ -1,55 +1,63 @@
 #include <iostream>
 #include <string>;
 #include <conio.h>
+#include <vector>
+#include <string>
+#include <map>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
+using std::vector;
+using PlayerCoordinates = std::pair<int, int>;
 
 const auto NROFELEMENTS = 10;
 const auto NROFROWS = 5;
 const auto LASTGAMEFIELD = NROFELEMENTS - 1;
 
-void printGame(int playerIndex)
+void printGame(const PlayerCoordinates& playerIndex)
 {
-    char gameState[NROFELEMENTS];
 
-    for (size_t i = 0; i < NROFELEMENTS; i++)
-        gameState[i] = '.';
+    auto&& game_board = vector<string>(NROFROWS, string(NROFELEMENTS, '.'));
 
-    gameState[0] = '|';
-    gameState[NROFELEMENTS -1] = '|';
-    gameState[playerIndex] = 'P';
+    game_board[0][0] = '|';
+    game_board[NROFROWS - 1][NROFELEMENTS - 1] = '|';
 
-    for (size_t i = 0; i < NROFELEMENTS; i++)
-        cout << gameState[i];
+    game_board[playerIndex.first][playerIndex.second] = 'P';
+
+    for (const auto& row : game_board)
+        cout << row << endl;
 
     cout << endl;
 }
 
-int movePlayer(int playerIndex)
+void movePlayer(PlayerCoordinates& playerIndex)
 {
     std::string userInput{};
-    cout << "Move (f)orward or (b)ackward?";
+    cout << "Move (wasd): ";
     cin >> userInput;
 
-    if (userInput == "f")
-        playerIndex++;
-    else if (userInput == "b" && playerIndex > 0)
-        playerIndex--;
+    if (userInput == "d") // right
+        playerIndex.second = ++playerIndex.second;
+    else if (userInput == "a") // left
+        playerIndex.second = --playerIndex.second;
+    else if (userInput == "w") // up
+        playerIndex.first = --playerIndex.first;
+    else if (userInput == "s") // down
+        playerIndex.first = ++playerIndex.first;
     else
         cout << "Unrecognized move!";
-
-    return playerIndex;
 }
 
-bool shouldEndgame(int playerIndex)
+bool shouldEndgame(const PlayerCoordinates& playerIndex)
 {
-    if (playerIndex != LASTGAMEFIELD)
-        return false;
-
-    cout << "Your reached the end";
-    return true;
+    if (playerIndex.second == LASTGAMEFIELD && playerIndex.first == NROFROWS - 1)
+    {
+        cout << "Your reached the end";
+        return true;
+    }
+    return false;
 }
 
 void clearScreen()
@@ -60,13 +68,13 @@ void clearScreen()
 int main()
 {
     auto quit = false;
-    auto playerIndex = 0;
+    auto&& playerIndex = PlayerCoordinates(0, 0);
     const auto lastGameField = NROFELEMENTS - 1;
 
     while (!quit)
     {
         printGame(playerIndex);
-        playerIndex = movePlayer(playerIndex);
+        movePlayer(playerIndex);
         clearScreen();
 
         quit = shouldEndgame(playerIndex);
