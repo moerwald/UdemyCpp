f88c8e8b6483fe16bbfd3a158720de5a7133d17d
moerwald <andreas.mewald@gmail.com>
2021-07-01 09:23:59 +0200

    Created map

--- src/7_Programmieraufgabe/main.cc
+++ src/7_Programmieraufgabe/main.cc
@@ -1,9 +1,15 @@
 
+#include "exercise.h"
 
 
 
 int main()
 {
+
+    Friends friends;
+
+    friends["Jan"] = std::pair<int, int> (36, 82);
+    friends["Ina"] = std::pair<int, int> (26, 52);
 
 
 	return 0;
