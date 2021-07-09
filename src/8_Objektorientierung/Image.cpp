0e9071c320f3950132322b36f62104c40d82a4eb
moerwald <andreas.mewald@gmail.com>
2021-07-09 09:10:09 +0200

    Added skeleton

--- src/8_Objektorientierung/Image.cpp
+++ src/8_Objektorientierung/Image.cpp
@@ -1,1 +1,5 @@
 #include "Image.h"
+
+Image::Image(const unsigned int& width, const unsigned int& height)
+{
+}
--- src/8_Objektorientierung/Image.h
+++ src/8_Objektorientierung/Image.h
@@ -1,5 +1,20 @@
 #pragma once
+
+#include <vector>
+
+
+using uchar = unsigned char;
+using GraySclaeMatrx = std::vector<std::vector<uchar>>;
+
 class Image
 {
+public:
+    Image(const unsigned int &width, const unsigned int &height);
+    ~Image() {};
+
+
+private:
+    unsigned int _width;
+    unsigned int _height;
 };
 
