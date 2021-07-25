2555a8c8ea9c999d561b2a617108f3fcb794c1f2
moerwald <andreas.mewald@gmail.com>
2021-07-25 08:20:15 +0200

    Lambda now captures local variable

--- src/10_LambdaFunktionen/Main.cpp
+++ src/10_LambdaFunktionen/Main.cpp
@@ -11,8 +11,19 @@ int main()
 
     std::vector<int> vec_copied;
     std::copy_if(vec.begin(),
-                 vec.end(),
-                 std::back_inserter(vec_copied),
-                 [](int val) { return val % 2 == 0 ? true : false; });
+        vec.end(),
+        std::back_inserter(vec_copied),
+        [](int val) { return val % 2 == 0 ? true : false; });
+
+    constexpr auto upper_bound = 5;
+    auto it_of_values_to_remove = std::remove_if(vec.begin(),
+                   vec.end(),
+                   [upper_bound](int val) {
+                        if (val < upper_bound)
+                            return true;
+                        return false;
+                   });
+
+    vec.erase(it_of_values_to_remove, vec.end());
     return 0;
 }
