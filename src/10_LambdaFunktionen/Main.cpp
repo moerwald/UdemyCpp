5f07bca9d08c40b73e13358c7dd84fddcdb984bf
moerwald <andreas.mewald@gmail.com>
2021-07-25 08:28:29 +0200

    Now printing vector content

--- src/10_LambdaFunktionen/Main.cpp
+++ src/10_LambdaFunktionen/Main.cpp
@@ -3,27 +3,40 @@
 #include <algorithm>
 #include <numeric>
 #include <iterator>
+#include <iostream>
 
 int main()
 {
+    auto print_vec = [](const std::vector<int>& v) {
+        for (auto& el : v)
+        {
+            std::cout << el << ", ";
+        }
+        std::cout << std::endl;
+    };
+
     std::vector<int> vec(10, 0);
     std::iota(vec.begin(), vec.end(), 0);
+
+    print_vec(vec);
 
     std::vector<int> vec_copied;
     std::copy_if(vec.begin(),
         vec.end(),
         std::back_inserter(vec_copied),
-        [](int val) { return val % 2 == 0 ? true : false; });
+        [](const int& val) { return val % 2 == 0 ? true : false; });
+    print_vec(vec_copied);
 
     constexpr auto upper_bound = 5;
     auto it_of_values_to_remove = std::remove_if(vec.begin(),
-                   vec.end(),
-                   [upper_bound](int val) {
-                        if (val < upper_bound)
-                            return true;
-                        return false;
-                   });
+        vec.end(),
+        [upper_bound](const int& val) {
+            if (val < upper_bound)
+                return true;
+            return false;
+        });
 
     vec.erase(it_of_values_to_remove, vec.end());
+    print_vec(vec);
     return 0;
 }
